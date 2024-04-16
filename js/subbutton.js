d837c9f3d45aeae8c3502f8375d8ba361f2c13e0
IntaHarshini <158248432+IntaHarshini@users.noreply.github.com>
2024-04-17 01:50:29 +0530

    Add files via upload

---
+++ js/subbutton.js
@@ -0,0 +1,64 @@
+//baaga beach
+function food(){ 
+    window.location.href="https://www.google.com/maps/search/Restaurants/@15.5562728,73.7467156,16z/data=!4m8!2m7!3m6!1sRestaurants!2sBaga+Beach!3s0x3bbfea1e743accd5:0xc68cf75c779eb61b!4m2!1d73.7492626!2d15.5621864?entry=ttu"
+}
+function hotela(){
+    window.location.href="https://www.google.com/maps/search/Hotels/@15.5792522,73.7263228,14z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbfe96efd4101af:0x1fe7bea632841b03!2sVagator+Beach,+Goa!2m2!1d73.733627!2d15.6029835!1m6!1m2!1s0x3bbfea1cd8a1deff:0xba7066c0ee35c1e0!2sBaga+Beach,+Goa+403519!2m2!1d73.7517307!2d15.5552787!2m7!3m6!1sHotels!2sBaga+Beach!3s0x3bbfea1cd8a1deff:0xba7066c0ee35c1e0!4m2!1d73.7492477!2d15.5622059?entry=ttu"
+}
+function loca(){
+    window.location.href="https://www.google.com/maps/dir//Baga+Beach,+Goa+403519/@15.5553576,73.7105309,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbfea1cd8a1deff:0xba7066c0ee35c1e0!2m2!1d73.7517307!2d15.5552787?entry=ttu"
+}
+
+//cola beach
+function funcb(){
+    window.location.href="https://www.google.com/maps/search/Restaurants/@15.0820288,73.9324585,14z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbfea1e743accd5:0xc68cf75c779eb61b!2sBaga+Beach!2m2!1d73.749294!2d15.5622086!1m6!1m2!1s0x3bbe4f035bfd7c19:0xe4d2a393154f9db4!2sCola+Beach+Rd,+Cola,+Goa!2m2!1d73.974458!2d15.0636978!2m7!3m6!1sRestaurants!2sCola+Beach+Rd!3s0x3bbe4f035bfd7c19:0xe4d2a393154f9db4!4m2!1d73.974458!2d15.0636978?entry=ttu"
+}
+function hotelb(){
+    window.location.href="https://www.google.com/maps/search/Hotels/@15.0573735,73.9348036,13z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbfea1cd8a1deff:0xba7066c0ee35c1e0!2sBaga+Beach!2m2!1d73.7517307!2d15.5552787!1m6!1m2!1s0x3bbe4f035bfd7c19:0xe4d2a393154f9db4!2sCola+Beach+Rd,+Cola,+Goa!2m2!1d73.974458!2d15.0636978!2m7!3m6!1sHotels!2sCola+Beach+Rd!3s0x3bbe4f035bfd7c19:0xe4d2a393154f9db4!4m2!1d73.974458!2d15.0636978?entry=ttu"
+}
+function locb(){
+    window.location.href="https://www.google.com/maps/dir//Cola+Beach,+Cola,+Goa/@15.0566336,73.9302173,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbe4f034b05b837:0xf52f19eaf1b9498a!2m2!1d73.9714171!2d15.0565544?entry=ttu"
+}
+//calangute beach
+function funcc(){
+    window.location.href="https://www.google.com/maps/search/Restaurants/@15.5722943,73.7370821,14z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbfe97d6e4b498f:0xe891136b039794d2!2sVagator!2m2!1d73.7515066!2d15.5952306!1m6!1m2!1s0x3bbfea0148f4ed2b:0xcb592fad5d257d17!2sCalangute+Beach,+Goa+403519!2m2!1d73.7534857!2d15.549441!2m7!3m6!1sRestaurants!2sCalangute+Beach!3s0x3bbfea0148f4ed2b:0xcb592fad5d257d17!4m2!1d73.7507276!2d15.5586628?entry=ttu"
+}
+function hotelc(){
+    window.location.href="https://www.google.com/maps/search/Hotels/@15.5481437,73.7494892,15z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbe4f035bfd7c19:0xe4d2a393154f9db4!2sCola+Beach+Rd!2m2!1d73.974458!2d15.0636978!1m6!1m2!1s0x3bbfea077c604cb5:0x1f7a02dc2a31813e!2sCalangute+Beach,+Calangute,+Goa+403519!2m2!1d73.7560598!2d15.545019!2m7!3m6!1sHotels!2sCalangute+Beach!3s0x3bbfea077c604cb5:0x1f7a02dc2a31813e!4m2!1d73.7560615!2d15.545008?entry=ttu"
+}
+function locc(){
+    window.location.href="https://www.google.com/maps/dir//Calangute+Beach,+Goa/@15.5495199,73.7122859,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbfea0148f4ed2b:0xcb592fad5d257d17!2m2!1d73.7534857!2d15.549441?entry=ttu"
+}
+//butterfly beach
+function funcd(){
+    window.location.href="https://www.google.com/maps/search/Restaurants/@15.0406786,74.0364251,12z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbfea1cd8a1deff:0xba7066c0ee35c1e0!2sBaga+Beach,+Goa+403519!2m2!1d73.7517307!2d15.5552787!1m6!1m2!1s0x3bbe4f8a16e887f1:0x894249fead443527!2sButterfly+Beach,+Goa+403702!2m2!1d74.001647!2d15.0196019!2m7!3m6!1sRestaurants!2sButterfly+Beach!3s0x3bbe4f8a16e887f1:0x894249fead443527!4m2!1d74.0017678!2d15.0197368?entry=ttu"
+}
+function hoteld(){
+    window.location.href="https://www.google.com/maps/search/Hotels/@15.011918,73.9742941,13z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbfea077c604cb5:0x1f7a02dc2a31813e!2sCalangute+Beach!2m2!1d73.7560598!2d15.545019!1m6!1m2!1s0x3bbe4f8a16e887f1:0x894249fead443527!2sButterfly+Beach,+Goa+403702!2m2!1d74.001647!2d15.0196019!2m7!3m6!1sHotels!2sButterfly+Beach!3s0x3bbe4f8a16e887f1:0x894249fead443527!4m2!1d74.0017678!2d15.0197368?entry=ttu"
+}
+function locd(){
+    window.location.href="https://www.google.com/maps/dir//Butterfly+Beach,+Goa/@15.0196811,73.9604472,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbe4f8a16e887f1:0x894249fead443527!2m2!1d74.001647!2d15.0196019?entry=ttu"
+}
+//vagator beach
+function funce(){
+    window.location.href="https://www.google.com/maps/search/Restaurants/@15.5961215,73.7380998,15z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbe4f8a16e887f1:0x894249fead443527!2sButterfly+Beach!2m2!1d74.001647!2d15.0196019!1m6!1m2!1s0x3bbfe97d6e4b498f:0xe891136b039794d2!2sVagator,+Goa+403519!2m2!1d73.7515066!2d15.5952306!2m7!3m6!1sRestaurants!2sVagator!3s0x3bbfe97d6e4b498f:0xe891136b039794d2!4m2!1d73.7515039!2d15.5952257?entry=ttu"
+}
+function hotele(){
+    window.location.href="https://www.google.com/maps/search/Hotels/@15.5939905,73.7381605,15z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbe4f8a16e887f1:0x894249fead443527!2sButterfly+Beach!2m2!1d74.001647!2d15.0196019!1m6!1m2!1s0x3bbfe97d6e4b498f:0xe891136b039794d2!2sVagator,+Goa+403519!2m2!1d73.7515066!2d15.5952306!2m7!3m6!1sHotels!2sVagator!3s0x3bbfe97d6e4b498f:0xe891136b039794d2!4m2!1d73.7515039!2d15.5952257?entry=ttu"
+}
+function loce(){
+    window.location.href="https://www.google.com/maps/dir//Vagator+Beach,+Goa/@15.6030623,73.6924272,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbfe96efd4101af:0x1fe7bea632841b03!2m2!1d73.733627!2d15.6029835?entry=ttu"
+}
+//candolim beach
+function funcf(){
+    window.location.href="https://www.google.com/maps/search/Restaurants/@15.5376018,73.7384869,14z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbfea1cd8a1deff:0xba7066c0ee35c1e0!2sBaga+Beach,+Goa!2m2!1d73.7517307!2d15.5552787!1m6!1m2!1s0x3bbfc1a3ca96d9fb:0xd4400f3dbaa7b588!2sCandolim,+Goa!2m2!1d73.7688876!2d15.5127952!2m7!3m6!1sRestaurants!2sCandolim!3s0x3bbfc1a3ca96d9fb:0xd4400f3dbaa7b588!4m2!1d73.768895!2d15.5127959?entry=ttu"
+}
+function hotelf(){
+    window.location.href="https://www.google.com/maps/search/Hotels/@15.5939905,73.7381605,15z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbe4f8a16e887f1:0x894249fead443527!2sButterfly+Beach!2m2!1d74.001647!2d15.0196019!1m6!1m2!1s0x3bbfe97d6e4b498f:0xe891136b039794d2!2sVagator,+Goa+403519!2m2!1d73.7515066!2d15.5952306!2m7!3m6!1sHotels!2sVagator!3s0x3bbfe97d6e4b498f:0xe891136b039794d2!4m2!1d73.7515039!2d15.5952257?entry=ttu"
+}
+function locf(){
+    window.location.href="https://www.google.com/maps/dir//Candolim+Beach,+Candolim+Beach+Road,+Candolim,+Goa/@15.5180538,73.7214124,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbfc13c227bd1bf:0xfa2f8c6c1efa4aef!2m2!1d73.7626122!2d15.5179749?entry=ttu"
+}
+document.querySelector(".contact").addEventListener("click", function (e) {e.preventDefault();
+document.querySelector(".footer").scrollIntoView({behavior: 'smooth'
+ });});
