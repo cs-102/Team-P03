0958fcaf419f8d394d8214f2b80680f014ad4caf
Gvyshnaviyadav <vyshumuni@gmail.com>
2024-04-16 22:40:07 +0530

    fi

---
+++ script1.js
@@ -0,0 +1,50 @@
+
+                                        function mine(){
+                                            window.location.href='https://www.google.com/maps/search/Restaurants/@15.4981311,73.8927542,14z/data=!3m1!4b1!4m22!1m13!4m12!1m4!2m2!1d73.971972!2d15.4131491!4e1!1m6!1m2!1s0x3bbfbef2b99b0289:0x1e8056689f15a7b8!2sBasilica+of+Bom+Jesus,+Old+Goa+Road,+Velha+Goa,+Goa+403402!2m2!1d73.9114101!2d15.5008427!2m7!3m6!1sRestaurants!2sBasilica+of+Bom+Jesus!3s0x3bbfbef2b99b0289:0x1e8056689f15a7b8!4m2!1d73.9114739!2d15.5011397?authuser=0&entry=ttu'
+                                        }
+                                        function mine1(){
+                                            window.location.href='https://www.google.com/maps/dir//Basilica+of+Bom+Jesus,+Old+Goa+Road,+Velha+Goa,+Goa+403402/@15.500829,73.9088312,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbfbef2b99b0289:0x1e8056689f15a7b8!2m2!1d73.9114101!2d15.5008427?authuser=0&entry=ttu'
+                                        }
+                                        function mine2(){
+                                            window.location.href='https://www.google.com/maps/dir/15.4138148,73.971972/Se+Cathedral,+Velha+Goa,+Goa/@15.4684455,73.8654981,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfbef32db44613:0xc94c4740248a9cf0!2m2!1d73.9122553!2d15.5036895?authuser=0&entry=ttu'
+                                        }
+                                        function mine3(){
+                                            window.location.href='https://www.google.com/maps/search/Restaurants/@15.4981311,73.8927542,14z/data=!3m1!4b1!4m22!1m13!4m12!1m4!2m2!1d73.971972!2d15.4138148!4e1!1m6!1m2!1s0x3bbfbef32db44613:0xc94c4740248a9cf0!2sSe+Cathedral,+Velha+Goa,+Goa!2m2!1d73.9122553!2d15.5036895!2m7!3m6!1sRestaurants!2sSe+Cathedral!3s0x3bbfbef32db44613:0xc94c4740248a9cf0!4m2!1d73.9122666!2d15.5031987?authuser=0&entry=ttu'
+                                        }
+                                        function mine4(){
+                                            window.location.href='https://www.google.com/maps/dir/15.4138148,73.971972/The+Our+Lady+On+The+Mount+Chapel,+Velha+Goa,+Goa/@15.4684455,73.8660738,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfbfe4bc319d39:0x1a23dc7b216e05c1!2m2!1d73.9233284!2d15.5044717?authuser=0&entry=ttu'
+                                        }
+                                        function mine5(){
+                                            window.location.href='https://www.google.com/maps/search/Restaurants/@15.4684493,73.9072772,13z/data=!3m1!4b1!4m22!1m13!4m12!1m4!2m2!1d73.971972!2d15.4138148!4e1!1m6!1m2!1s0x3bbfbfe4bc319d39:0x1a23dc7b216e05c1!2sThe+Our+Lady+On+The+Mount+Chapel,+Velha+Goa,+Goa!2m2!1d73.9233284!2d15.5044717!2m7!3m6!1sRestaurants!2sThe+Our+Lady+On+The+Mount+Chapel!3s0x3bbfbfe4bc319d39:0x1a23dc7b216e05c1!4m2!1d73.9232252!2d15.5044491?authuser=0&entry=ttu'
+                                        }
+                                        function mine6(){
+                                            window.location.href='https://www.google.com/maps/dir/15.4138148,73.971972/St.+Cajetan+Church,+Goa,+Velha+Goa,+Goa/@15.4684455,73.8660738,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfbe8b7f6d3a3f:0x97184bcf84c55e62!2m2!1d73.9150423!2d15.5055969?authuser=0&entry=ttu'
+                                        }
+                                        function mine7(){
+                                            window.location.href='https://www.google.com/maps/search/Restaurants/@15.4981311,73.8927542,14z/data=!3m1!4b1!4m22!1m13!4m12!1m4!2m2!1d73.971972!2d15.4138148!4e1!1m6!1m2!1s0x3bbfbe8b7f6d3a3f:0x97184bcf84c55e62!2sSt.+Cajetan+Church,+Goa,+Velha+Goa,+Goa!2m2!1d73.9150423!2d15.5055969!2m7!3m6!1sRestaurants!2sSt.+Cajetan+Church,+Goa!3s0x3bbfbe8b7f6d3a3f:0x97184bcf84c55e62!4m2!1d73.914981!2d15.5055333?authuser=0&entry=ttu'
+                                        }
+                                        function mine8(){
+                                            window.location.href='https://www.google.com/maps/dir/15.4138148,73.971972/Mae+De+Deus+Church,+Chogm+Road,+Muddavaddi,+Saligao,+Goa/@15.4807092,73.8043658,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfc1d7d820ba15:0xdf203e5cb919d95!2m2!1d73.7853113!2d15.5470308?authuser=0&entry=ttu'
+                                        }
+                                        function mine9(){
+                                            window.location.href='https://www.google.com/maps/search/Restaurants/@15.5508381,73.7567808,14z/data=!3m1!4b1!4m22!1m13!4m12!1m4!2m2!1d73.971972!2d15.4138148!4e1!1m6!1m2!1s0x3bbfc1d7d820ba15:0xdf203e5cb919d95!2sMae+De+Deus+Church,+Chogm+Road,+Muddavaddi,+Saligao,+Goa!2m2!1d73.7853113!2d15.5470308!2m7!3m6!1sRestaurants!2sMae+De+Deus+Church!3s0x3bbfc1d7d820ba15:0xdf203e5cb919d95!4m2!1d73.785382!2d15.5473579?authuser=0&entry=ttu'
+                                        }
+                                        function mine10(){
+                                            window.location.href='https://www.google.com/maps/dir/15.4138148,73.971972/Campus+of+Church+of+St.+Francis+of+Assisi,+Velha+Goa,+Goa/@15.4587434,73.906373,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfbf9ebc67f737:0x45ef29c67275906b!2m2!1d73.9117341!2d15.5023199?authuser=0&entry=ttu'
+                                        }
+                                        function mine11(){
+                                            window.location.href='https://www.google.com/maps/search/Restaurants/@15.4589013,73.9063729,13z/data=!3m1!4b1!4m22!1m13!4m12!1m4!2m2!1d73.971972!2d15.4138148!4e1!1m6!1m2!1s0x3bbfbf9ebc67f737:0x45ef29c67275906b!2sCampus+of+Church+of+St.+Francis+of+Assisi,+Velha+Goa,+Goa!2m2!1d73.9117341!2d15.5023199!2m7!3m6!1sRestaurants!2sCampus+of+Church+of+St.+Francis+of+Assisi!3s0x3bbfbf9ebc67f737:0x45ef29c67275906b!4m2!1d73.9115907!2d15.5022528?authuser=0&entry=ttu'
+                                            
+                                        }
+                                        function mine12(){
+                                            window.location.href="https://www.google.com/maps/search/Restaurants/@15.5677829,73.7893838,14z/data=!3m1!4b1!4m23!1m15!4m14!1m6!1m2!1s0x3bbe4f8a16e887f1:0x894249fead443527!2sButterfly+Beach,+Goa!2m2!1d74.001647!2d15.0196019!1m6!1m2!1s0x3bbfea8f6df32ca9:0x26da8bea2cea101e!2sGuirim,+Goa+403507!2m2!1d73.8034299!2d15.5614189!2m6!3m5!2sSt.+Diogo's+Church!3s0x3bbfea8f6df32ca9:0x26da8bea2cea101e!4m2!1d73.8036241!2d15.5616768?entry=ttu"
+                                        }
+                                        function mine13(){
+                                            window.location.href='https://www.google.com/maps/dir//Guirim,+Goa+403507/@15.5614036,73.7210281,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbfea8f6df32ca9:0x26da8bea2cea101e!2m2!1d73.8034299!2d15.5614189?entry=ttu'
+                                        }
+                                        function mine15(){
+                                            window.location.href='https://www.google.com/maps/dir//GW36%2B953,+Velha+Goa,+Goa+403402/@15.5033597,73.8280332,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbfbef310fbc135:0x82266829954a5497!2m2!1d73.910435!2d15.503375?entry=ttu'
+                                        }
+                                        function mine14(){
+                                            window.location.href='https://www.google.com/maps/search/Restaurants/@15.4995468,73.9021078,15z/data=!3m1!4b1!4m24!1m15!4m14!1m6!1m2!1s0x3bbe4f8a16e887f1:0x894249fead443527!2sButterfly+Beach,+Goa!2m2!1d74.001647!2d15.0196019!1m6!1m2!1s0x3bbfbef310fbc135:0x82266829954a5497!2sGW36%2B953,+Velha+Goa,+Goa+403402!2m2!1d73.910435!2d15.503375!2m7!3m6!1sRestaurants!2sChapel+of+St.+Catherine+of+Alexandria!3s0x3bbfbef310fbc135:0x82266829954a5497!4m2!1d73.9103464!2d15.5038063?entry=ttu'
+                                        }
