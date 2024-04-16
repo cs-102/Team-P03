b83772604f36d79ccc6b2ea6441413d1e9859f02
MrGauravDewangan <158248009+MrGauravDewangan@users.noreply.github.com>
2024-04-17 00:41:19 +0530

    Updating vehicles and bikes

---
+++ js/vehicles.js
@@ -0,0 +1,8 @@
+function carmove(){
+    document.getElementById("car").classList.replace("car","carmove");
+    document.getElementById("carspan").classList.replace("carspan","carspanmv");
+}
+function bikemove(){
+    document.getElementById("bike").classList.replace("bike","bikemove");
+    document.getElementById("bikespan").classList.replace("bikespan","bikespanmv");
+}
